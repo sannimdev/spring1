d099b9284d221a52c29ba36fee7ddb2b3c6c3d2c
Sannim <inegg.apps@gmail.com>
2020-07-28 16:50:25 +0900

    시큐리티 연봉 중...

---
+++ src/main/java/com/spring1/security/LoginSuccessHandler.java
@@ -0,0 +1,10 @@
+package com.spring1.security;
+
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler{
+	
+	
+	
+	
+}
